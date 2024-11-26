914971842680b4608411d04365c88c0ff6e3b9d8
00015955 <00015955wiut@gmail.com>
2024-11-26 23:32:02 +0500

    feat: buttons created

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e98b3c9a4e674011a7052c11242e5bcdb7a28880")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a80e229ace028442af715fa9d41b03cc618c5cf1")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

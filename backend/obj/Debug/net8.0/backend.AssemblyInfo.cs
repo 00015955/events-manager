299593fdbe78f87c73f0ee58b108582c74a1a04e
00015955 <00015955wiut@gmail.com>
2024-12-05 20:41:18 +0500

    chore: final commit hopefully

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e89563687207ab887e8f0dbd1c9e7bcaea7a5903")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3dc1f1fca45ed170cbfc85e7e1d10e3ef2e76e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

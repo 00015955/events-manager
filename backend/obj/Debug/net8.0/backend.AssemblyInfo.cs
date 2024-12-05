b72b9f399e385b4c7b6e81971f0db4784bcd7a37
00015955 <00015955wiut@gmail.com>
2024-12-05 19:22:14 +0500

    fix: some fixes

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79db95af573ed99c3d7e930a28f7d25bdd0f042d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43638ef492a03b7d57b89f72294ae8e1d1d7c356")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
